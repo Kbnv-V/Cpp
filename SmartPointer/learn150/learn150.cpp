f84693d5faae6d8fa01243ef8ca356429cd96a5e
sun_arr <115037299+Kbnv-V@users.noreply.github.com>
2026-05-14 09:25:18 +0300

    Add files via upload

---
+++ SmartPointer/learn150/learn150.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+//умные указатели
+//для автоматической очистки указателя надо создавать класс, который это будет делать
+//это и есть умный указатель
+
+template<typename T>
+class SmartPointer
+{
+public:
+    SmartPointer(T *value)
+    {
+        this->ptr = value;
+    }
+
+    ~SmartPointer()
+    {
+        delete ptr;
+    }
+
+    T& operator *()
+    {
+        return *ptr;
+    }
+
+private:
+    T* ptr;
+
+};
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    /*
+    int* ptr = new int(5);
+    delete ptr;
+    */
+
+    SmartPointer<int> pointer = new int(10);
+
+    cout << *pointer << endl;
+}
