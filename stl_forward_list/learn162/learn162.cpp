0d2fa595cc5bc5654f7672f594a7e56186d9cfb1
sun_arr <115037299+Kbnv-V@users.noreply.github.com>
2026-06-09 19:30:40 +0300

    Add files via upload

---
+++ stl_forward_list/learn162/learn162.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <forward_list>
+using namespace std;
+
+//forward_list - для реализации односвязных списков
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    forward_list<int> fl;
+
+    fl.push_front(111);
+    fl.push_front(222);
+
+    forward_list<int>::iterator it = fl.begin();
+    // it++;
+    //cout << *it << endl;
+
+    fl.insert_after(it, 999);
+
+    for (auto var : fl)
+    {
+        cout << var << endl;
+    }
+}
