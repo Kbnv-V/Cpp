0efb887d7f857b7bb8b9fa7322ec0546fde6325e
sun_arr <115037299+Kbnv-V@users.noreply.github.com>
2026-06-17 19:40:39 +0300

    Add files via upload

---
+++ thread/learn169/learn169.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono> //для работы со временем
+using namespace std;
+
+//многопоточное программирование. thread
+
+void doWork()
+{
+    for (size_t i = 0; i < 10; i++)
+    {
+        cout << "ID потока: " << this_thread::get_id() << " DoWork " << i << endl;
+        this_thread::sleep_for(chrono::milliseconds(1000)); //прост.имен chorno тут используется для паузы
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    thread th(doWork); //чтобы выполнять параллельно другую задачу, надо создать экземпляк thread и передать в него указатель на функцию (без скобок)
+    thread th1(doWork);
+
+    //th.detach(); //разрывает связь между объектом th и потоком, который выполняется
+
+    //doWork();
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        cout << "ID потока: " << this_thread::get_id() << " main " << i << endl; //синтаксис изпользование метода из пространства имен. Тут получаем ID потока
+        this_thread::sleep_for(chrono::milliseconds(500)); //прост.имен chorno тут используется для паузы. приостанавливает выполнение потока
+    }
+
+    th.join(); //позволяет дожитаться выполнения задачи
+    th1.join();
+    //метод join надо вызывать в том месте, где нужно дождаться выполнения
+}
