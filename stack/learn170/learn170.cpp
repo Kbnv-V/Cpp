3fda59473920ae03983598c0beac5e1554a07738
sun_arr <115037299+Kbnv-V@users.noreply.github.com>
2026-06-17 20:16:04 +0300

    Add files via upload

---
+++ stack/learn170/learn170.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+#include <stack>
+using namespace std;
+
+//STL - stack. Адаптер контейнеров
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    stack<int, deque<int>> newStack; //синтаксис с определением типа данных и какой контейнер будем использовать
+
+    newStack.push(1); //сначала создается объект, потом перемещается в коллекцию(stack)
+    newStack.push(2);
+    newStack.push(3);
+
+    newStack.emplace(4); //создает объект сразу в коллекции. Этот метод быстрее
+
+    auto a = newStack._Get_container(); //метод для извлечения контейнера. После извлевлечения можно использовать базовые методы контейнера, который мы задали выше
+
+    cout << a[2] << endl;
+
+    cout << "Размер стека: " << newStack.size() << endl;
+    cout << "Метод pop: " << newStack.top() << endl;
+
+    while (!newStack.empty())
+    {
+        cout << newStack.top() << endl;
+        newStack.pop();
+    }
+}
