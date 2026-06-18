776a936813fc9e41278a74c903bcaa22b711b94c
sun_arr <115037299+Kbnv-V@users.noreply.github.com>
2026-06-18 19:05:38 +0300

    Add files via upload

---
+++ thread_param/learn171/learn171.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+void doWork(int a, int b, string message)
+{
+    this_thread::sleep_for(chrono::milliseconds(2000));
+    cout << "Начало работы функции doWork" << endl;
+    this_thread::sleep_for(chrono::milliseconds(2000));
+    cout << "Параметр a - " << a << endl;
+    cout << "Параметр b - " << b << endl;
+    this_thread::sleep_for(chrono::milliseconds(2000));
+    cout << "Сумма параметров a и b - " << a + b << endl;
+    this_thread::sleep_for(chrono::milliseconds(2000));
+    cout << "Выпол сообщения - " << message << endl;
+    this_thread::sleep_for(chrono::milliseconds(2000));
+    cout << "Конец работы функции doWork" << endl;
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    //doWork(1240, 54122);
+
+    thread newTh(doWork, 100, 150, "Тестовое сообщение"); //синтаксис передачи параметров в поток
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        cout << "ID потока - " << this_thread::get_id() << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+    }
+
+    newTh.join();
+}
